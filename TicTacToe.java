0ba6038cf5700b1ada0627c868165c9560ab7a6c
Shouryaditya Dhanraj Singh <shouryaditya.ds@gmail.com>
2026-04-20 13:41:19 +0530

    Coded UC2: Toss to Decide First Player and Symbol

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,46 +1,38 @@
-// TicTacToe
-// This program initializes and displays an empty Tic-Tac-Toe board in a proper
-// grid format. This use case introduces 2D arrays, nested loops,
-// and formatted console output.
+import java.util.Random;
 
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
-
-    /**
-     * Entry point of the program. It initializes the board and prints
-     * the empty grid on the console.
-     */
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    /**
-     * Initializes the 3x3 board by filling each cell with '-' to indicate
-     * an empty position. Students should focus on correct nested loop usage.
-     */
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
+    static void tossAndAssignSymbols() {
+        Random r1 = new Random();
+        int r2 = r1.nextInt(2);
+        if(r2 == 0) {
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+            isHumanTurn = true;
+        }
+        else {
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+            isHumanTurn = false;
         }
     }
 
-    /**
-     * Prints the Tic-Tac-Toe board using horizontal and vertical separators
-     * so that the grid structure is clearly visible to the user.
-     */
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and will play first.");
+        } else {
+            System.out.println("Computer won the toss and will play first.");
         }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
     }
 }
